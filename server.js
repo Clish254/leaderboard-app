7b69405b81f3f5dce93167fddba31e719e6045c3
clish254 <clishilla@gmail.com>
2020-06-22 20:04:32 +0300

    fixed bug

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 3000;
 //opening a stream
 const readStream = fs.createReadStream('leaderboard.txt', 'utf8');
 //route to send tata
-app.get('/',(req,res)=> {
+app.get('/data',(req,res)=> {
     //reading a stream and sending the data from the stream to the front end
     readStream.on('data', function(chunk) {
         res.send(chunk)
