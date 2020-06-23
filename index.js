e8648c34755c1880c8b72fecde208900a504ff6c
clish254 <clishilla@gmail.com>
2020-06-23 09:47:12 +0300

    fixed bug

--- index.js
+++ index.js
@@ -26,34 +26,79 @@
     },
     ];*/
    // Example POST method implementation:
+   let parsed = null;
+   fetch('https://leaderboard-api-hng.herokuapp.com/data')
+   .then(response => response.json())
+   .then(data => {
+     parsed =data;
+    
+        // EXTRACT VALUE FOR HTML HEADER. 
+        // ('Book ID', 'Book Name', 'Category' and 'Price')
+        var col = [];
+        for (var i = 0; i < parsed.length; i++) {
+            for (var key in parsed[i]) {
+                if (col.indexOf(key) === -1) {
+                    col.push(key);
+                }
+            }
+        }
+        shareField = 'share'
+        col.push(shareField)
+        // CREATE DYNAMIC TABLE.
+        var table = document.createElement("table");
 
-/*// (C) TABLE HEADER
-var theWrap = document.getElementById("tableWrap");
-var theCell = null;
-for (let key in parsed[0]) {
-  theCell = document.createElement("div");
-  theCell.innerHTML = key;
-  theCell.classList.add("cell");
-  theCell.classList.add("head");
-  theWrap.appendChild(theCell);
-}
+        // CREATE HTML TABLE HEADER ROW USING THE EXTRACTED HEADERS ABOVE.
 
-// (D) TABLE CELLS
-var thePerson = null;
-var altRow = false;
-for (let key in parsed) {
-  thePerson = parsed[key];
-  for (let i in thePerson) {
-    theCell = document.createElement("div");
-    theCell.innerHTML = thePerson[i];
-    theCell.classList.add("cell");
-    if (altRow) { 
-      theCell.classList.add("alt"); 
-    }
-    theWrap.appendChild(theCell);
+        var tr = table.insertRow(-1);                   // TABLE ROW.
+
+        for (var i = 0; i < col.length; i++) {
+            var th = document.createElement("th");      // TABLE HEADER.
+            th.innerHTML = col[i];
+            tr.appendChild(th);
+        }
+
+        // ADD JSON DATA TO THE TABLE AS ROWS.
+        for (var i = 0; i < parsed.length; i++) {
+
+            tr = table.insertRow(-1);
+
+            for (var j = 0; j < col.length; j++) {
+                var tabCell = tr.insertCell(-1);
+                tabCell.innerHTML = parsed[i][col[j]];
+            }
+        }
+
+        // FINALLY ADD THE NEWLY CREATED TABLE WITH JSON DATA TO A CONTAINER.
+        var divContainer = document.getElementById("showData");
+        divContainer.innerHTML = "";
+        divContainer.appendChild(table);
+        
+    });
+
+    function socialWindow(url) {
+      var left = (screen.width - 570) / 2;
+      var top = (screen.height - 570) / 2;
+      var params = "menubar=no,toolbar=no,status=no,width=570,height=570,top=" + top + ",left=" + left;
+      window.open(url,"NewWindow",params);
   }
-  altRow = !altRow;
-}
-  */
-
-
+  
+  function setShareLinks() {
+      var pageUrl = encodeURIComponent(document.URL);
+      var tweet = encodeURIComponent(jQuery("meta[property='og:description']").attr("content"));
+      
+      jQuery(".social-share.facebook").on("click", function() {
+          url = "https://www.facebook.com/sharer.php?u=" + pageUrl;
+          socialWindow(url);
+      });
+  
+      jQuery(".social-share.twitter").on("click", function() {
+          url = "https://twitter.com/intent/tweet?url=" + pageUrl + "&text=" + tweet;
+          socialWindow(url);
+      });
+  
+      jQuery(".social-share.linkedin").on("click", function() {
+          url = "https://www.linkedin.com/shareArticle?mini=true&url=" + pageUrl;
+          socialWindow(url);
+      })
+  }
+  
